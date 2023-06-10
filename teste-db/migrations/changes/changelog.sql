01ab6ab64dc44446bdabe9987e5979f986ae0673
Fabiano Furtado de Melo <fabianofm1975@gmail.com>
2023-06-10 20:41:02 -0300

    x

--- teste-db/migrations/changes/changelog.sql
+++ teste-db/migrations/changes/changelog.sql
@@ -8,7 +8,7 @@ CREATE TABLE testTable(
 
 --changeset stevedonie:create-test-table2
 CREATE TABLE testTable2(
-  columnName1 VARCHAR (300)
+  columnName1 VARCHAR (355)
 );
 --rollback DROP TABLE
 --rollback testTable2
