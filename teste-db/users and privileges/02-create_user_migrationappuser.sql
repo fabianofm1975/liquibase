0e888a9e8c83fe8c89d670d4e8479be9de26a8fd
Fabiano Furtado de Melo <fabianofm1975@gmail.com>
2023-06-10 14:13:46 -0300

    f

--- teste-db/users and privileges/02-create_user_migrationappuser.sql
+++ teste-db/users and privileges/02-create_user_migrationappuser.sql
@@ -1,6 +1,6 @@
 -- Executar em qualquer banco com usuário POSTGRES
 
-CREATE USER migrationappuser WITH ENCRYPTED PASSWORD 'Stone@2023!';
+CREATE USER migrationappuser WITH PASSWORD 'Stone@2023!';
 GRANT CONNECT ON DATABASE "teste-db" TO migrationappuser;
 
 -- Executar no BANCO DA APLICAÇÃO logado com usuário POSTGRES
