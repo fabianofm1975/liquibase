6f0f5501f66fa3507a928c84678432c548fd4558
Fabiano Furtado de Melo <fabianofm1975@gmail.com>
2023-06-10 20:16:42 -0300

    teste

--- teste-db/migrations/changes/postgres.changelog.sql
+++ teste-db/migrations/changes/postgres.changelog.sql
@@ -1,7 +1,7 @@
---changeset Fabiano Melo id:PR 001
-
-create table fabiano ();
-
---changeset author:Fabiano Melo id:PR 002
-
-create table fabiano2 ();
+--liquibase formatted sql
+--changeset stevedonie:create-test-table
+CREATE TABLE testTable(
+  columnName1 VARCHAR (355)
+);
+--rollback DROP TABLE
+--rollback testTable
