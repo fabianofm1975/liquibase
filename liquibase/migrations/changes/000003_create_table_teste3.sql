19a3ddd1b851427912b33f3ed5e42b9178c3d493
Fabiano Furtado de Melo <fabianofm1975@gmail.com>
2023-06-19 17:21:47 -0300

    changes

--- liquibase/migrations/changes/000003_create_table_teste3.sql
+++ liquibase/migrations/changes/000003_create_table_teste3.sql
@@ -1,5 +1,5 @@
 -- liquibase formatted sql			
--- changeset Fabiano:3 labels:label1,label2,label3 comment:coment1
+-- changeset Fabiano:3 labels:label1,label2,label3 comments:comment1,comment2,comment3
 
 create table if not exists teste3
 (
