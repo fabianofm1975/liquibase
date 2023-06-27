5d7a97c571b48730517e918bc8d51f1162675a97
Fabiano Furtado de Melo <fabianofm1975@gmail.com>
2023-06-26 21:04:29 -0300

    ajustes

--- liquibase/migrations/changes/000002_create_table_teste2.sql
+++
@@ -1,7 +0,0 @@
--- liquibase formatted sql			
--- changeset Fabiano:2 labels:label1,label2,label3 
-
-create table if not exists teste2
-(
-	nome varchar(20)
-);
--- liquibase/migrations/changes/000003_create_table_teste3.sql
+++
@@ -1,7 +0,0 @@
--- liquibase formatted sql			
--- changeset Fabiano:3 labels:label1,label2,label3 
-
-create table if not exists teste3
-(
-	nome varchar(20)
-);
